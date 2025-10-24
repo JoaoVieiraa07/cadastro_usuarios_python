9560d285c5d58c201a8e6fbd1c01b5aaccffcfc7
João Vitor da Silva Vieira <joao.svieira@selbetti.local>
2025-10-24 16:56:53 -0300

    modelo da tabela

---
+++ models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String  #Serve para importar a coluna, números inteiros, e as strings.
+from .database import Base #Função base que já havia sido criada no database
+class User(Base): #aqui eu chamo aquela função base para a tabela user
+    __tablename__ = "users" #nome da tabela
+
+    id = Column(Integer, primary_key=True) #id da conta/contato, e por ai vai
+    name = Column(String, index=True) 
+    email = Column(String, unique=True, index=True)
+    telephone = Column(Integer, unique=True, index=True)
