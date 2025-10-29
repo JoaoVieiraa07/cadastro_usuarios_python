3f5b4c1b581c1762f944bf60e794cb7d1def2d2c
João Vitor da Silva Vieira <joao.svieira@selbetti.com.br>
2025-10-29 14:13:36 -0300

    Criação da validação do email para o PUT

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from . import crud, models, schema as schemas
-from .database import engine, get_db
+from . database import engine, get_db
 from typing import List
 
 models.Base.metadata.create_all(bind=engine)
@@ -16,17 +16,17 @@ def criar_usuario(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.criar_usuario(db=db, user=user)
 
 @app.get("/users/{user_id}", response_model=schemas.UserResponse)
-def ver_usuario(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.pegar_id(db, user_id=user_id)
+async def ver_usuario(user_id: int, db: Session = Depends(get_db)):
+    db_user = await crud.pegar_id(db, user_id=user_id)
     if db_user is None:
-        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+        raise HTTPException(status_code=404, detail="Nenhum usuário cadastrado")
     return db_user
 
-@app.get("/users",response_model=List[schemas.UserResponse])
+@app.get("/users", response_model=List[schemas.UserResponse])
 def listar_usuarios(db: Session = Depends(get_db)):
     db_user = db.query(models.User).all()
-    if db_user is None:
-        raise HTTPException(status_code=404, detail = "Nenhum usuário encontrado")
+    if not db_user:
+        raise HTTPException(status_code=404, detail="Nenhum usuário encontrado")
     return db_user
 
 @app.put("/users/{user_id}")
@@ -34,6 +34,10 @@ def atualizar_usuario(user_id: int, user: schemas.UserUpdate, db: Session = Depe
     db_user = crud.pegar_id(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="Usuário não encontrado")
+    if user.email:
+        email_existente = crud.pegar_email(db, email=user.email)
+        if email_existente: 
+            raise HTTPException (status_code=400, detail="Email já cadastrado")
     return crud.atualizar_usuario(db=db, user_id=user_id, user=user)
 
 @app.delete("/users/{user_id}")
